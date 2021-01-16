ab73a15b56be5e7b40384eb0dc735bfc3505fe09
Christophe Noël <touffi51@gmail.com>
2021-01-16 08:40:25 +0100

    refactor: extract degrees definitions to own file for better re-usability

---
+++ tests/allDegrees.js
@@ -0,0 +1,28 @@
+const allDegrees = {};
+
+allDegrees.omit3 = { value: '3', type: 'subtract' };
+allDegrees.omitb3 = { value: '3', alter: '-1', type: 'subtract' };
+allDegrees.omit5 = { value: '5', type: 'subtract' };
+
+allDegrees.add3 = { value: '3', type: 'add' };
+allDegrees.add4 = { value: '4', type: 'add' };
+allDegrees.add6 = { value: '6', type: 'add' };
+allDegrees.addma7 = { value: '7', type: 'add' };
+allDegrees.addb9 = { value: '9', alter: '-1', type: 'add' };
+allDegrees.add9 = { value: '9', type: 'add' };
+allDegrees.addx9 = { value: '9', alter: '1', type: 'add' };
+allDegrees.add11 = { value: '11', type: 'add' };
+allDegrees.addx11 = { value: '11', alter: '1', type: 'add' };
+allDegrees.addb13 = { value: '13', alter: '-1', type: 'add' };
+allDegrees.add13 = { value: '13', type: 'add' };
+
+allDegrees.alterb5 = { value: '5', alter: '-1', type: 'alter' };
+allDegrees.alterx5 = { value: '5', alter: '1', type: 'alter' };
+allDegrees.alterb9 = { value: '9', alter: '-1', type: 'alter' };
+allDegrees.alterx9 = { value: '9', alter: '1', type: 'alter' };
+allDegrees.alterx11 = { value: '11', alter: '1', type: 'alter' };
+allDegrees.alterb13 = { value: '13', alter: '-1', type: 'alter' };
+
+export default allDegrees;
+
+//todo: all allNotes
