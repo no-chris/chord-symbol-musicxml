c2d4066f94f3cdb342dc7a0b55016f164b572fcd
Christophe Noël <touffi51@gmail.com>
2021-01-19 18:48:48 +0100

    build: Add build pipeline

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+/* eslint-env node */
+const path = require('path');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+	.BundleAnalyzerPlugin;
+
+const buildDir = 'lib';
+
+const config = {
+	target: 'web',
+	mode: 'production',
+	devtool: 'source-map',
+
+	entry: {
+		'chord-symbol-musicxml': './src/index.js',
+	},
+
+	output: {
+		filename: '[name].js',
+		path: path.resolve(process.cwd(), buildDir),
+		library: 'chord-symbol-musicxml',
+		libraryTarget: 'umd',
+		// https://github.com/webpack/webpack/pull/8625
+		globalObject: "typeof self !== 'undefined' ? self : this",
+	},
+
+	optimization: {
+		minimize: false,
+	},
+
+	performance: {
+		hints: false,
+	},
+
+	plugins: [
+		new CleanWebpackPlugin(),
+		new BundleAnalyzerPlugin({
+			analyzerMode: 'static',
+			openAnalyzer: false,
+		}),
+	],
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
+		],
+	},
+};
+
+module.exports = config;
